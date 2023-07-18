5bd290801f68e65e675139b00efaabca1776e3b8
seihoukei <a@zefiris.su>
2023-07-18 14:03:04 +0300

    Registered UIBarTooltip under new config structure

--- src/config/registered-ui-elements.js
+++ src/config/registered-ui-elements.js
@@ -1,9 +1,10 @@
 import UIMenuDialog from "components/ui/dialogs/menu/UIMenuDialog.svelte"
 import UISettingsDialog from "components/ui/dialogs/settings/UISettingsDialog.svelte"
+import UIBarTooltip from "components/ui/tooltips/UIBarTooltip.svelte"
 
 const REGISTERED_UI_ELEMENTS = {
     tooltips: {
-    
+        "bar": UIBarTooltip,
     },
     
     dialogs: {
